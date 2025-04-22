37d8c9786a02d43147d0fe55043651042c3c5b2a
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:06:14 +0300

    Add files via upload

---
+++ class1.1/class1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.WriteLine("1.1 Поменять местами две переменные");
+//Вводим с консоли переменные a и b
+Console.WriteLine("Введите переменную a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Меняем переменные местами:");
+a = a + b;
+b = a - b;
+a = a - b;
+Console.WriteLine($"Переменная a = {a}, переменная b = {b}");
