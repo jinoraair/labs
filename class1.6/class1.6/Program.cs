f49b9dfb7ccf0812bc96dc59732ea582317df1a0
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:10:57 +0300

    Add files via upload

---
+++ class1.6/class1.6/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+Console.WriteLine("1.6 Вывести таблицу значений функции f(x)=sin(x^2)*ln(x) для x от 10 до 15 с шагом 0.5");
+for (double x = 10; x <= 15; x += 0.5)
+{
+    double f = Math.Sin(Math.Pow(x, 2)) * Math.Log(x);
+    Console.WriteLine($"Для x = {x} f(x) = {f}");
+}
