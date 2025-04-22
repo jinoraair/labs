d268813b5f62c87f9e214b2f673b5f6884909325
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:11:50 +0300

    Add files via upload

---
+++ class1.7/class1.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+Console.WriteLine("1.7 Ввести вещ.число, знак арифм.операции и еще одно вещ.число. Вычислить и вывести результат.");
+Console.WriteLine("Введите первое вещественное число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите знак операции: ");
+string sign = Console.ReadLine();
+Console.WriteLine("Введите второе вещественное число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+if (sign == "+")
+{
+    Console.WriteLine(num1+num2);
+}
+if (sign == "-")
+{
+    Console.WriteLine(num1 - num2);
+}
+if (sign == "*")
+{
+    Console.WriteLine(num1 * num2);
+}
+if (sign == "/")
+{
+    Console.WriteLine(num1 + num2);
+}
+if (sign == "^")
+{
+    Console.WriteLine(Math.Pow(num1, num2));
+}
