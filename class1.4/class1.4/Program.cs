a50bfdba0bd2882f800d317506b60224218b954e
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:09:40 +0300

    Add files via upload

---
+++ class1.4/class1.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+Console.WriteLine("1.4 Ввести 3 вещ.числа и вывести значение выражения...");
+//Вводим переменную a
+Console.WriteLine("Введите вещественную переменную a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+//Вводим переменную b
+Console.WriteLine("Введите вещественную переменную b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+//Вводим переменную c
+Console.WriteLine("Введите вещественную переменную c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+//Находим значение выражения
+double result = ((Math.Pow(a * b + 7 * c,3)-(Math.Pow(a,2)+ Math.Pow(b, 2) + Math.Pow(c, 2))) / (b - a*c - a* (b+c)));
+Console.WriteLine($"Результат вычисления = {result}");
