0c59a449721e26edf994979f8c373412b0e5d134
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:08:26 +0300

    Add files via upload

---
+++ class1.2/class1.2/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+Console.WriteLine("1.2 Переставить цифры натурального числа таким образом, чтобы\nполучилось наименьшее число, записанное этими же цифрами");
+Console.WriteLine("Введите любое натуральное число: ");
+string num = Console.ReadLine();
+num = String.Concat(num.OrderBy(c => c));
+Console.WriteLine("Полученное натуральное число: ");
+Console.WriteLine(num);
