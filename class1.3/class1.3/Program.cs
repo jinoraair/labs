ce14c6ea2fcbd7441f716baf2810550a7463eaab
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:09:03 +0300

    Add files via upload

---
+++ class1.3/class1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+Console.WriteLine("Программа считает факториалы чисел до заданного числа.");
+Console.WriteLine("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int num = n;
+while (num > 0)
+{
+    n = 1;
+    for (int i = num; i>0; i--){
+        n *= i;
+    };
+    Console.WriteLine("Factorial of {0}! = {1}\n", num,n);
+    num--;
+}
