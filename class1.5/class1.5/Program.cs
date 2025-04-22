619b62fd9a8c21da2c854b79aec4619403cded31
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-22 23:10:19 +0300

    Add files via upload

---
+++ class1.5/class1.5/Program.cs
@@ -0,0 +1,4 @@
+﻿using System;
+Console.WriteLine("1.5 Вычислить значение выражения");
+double result = (Math.Sin(5)+Math.Pow(1.75, 2)) / (3*Math.Pow(double.Epsilon,Math.Cos(7)));
+Console.WriteLine($"Результат вычисления = {result}");
