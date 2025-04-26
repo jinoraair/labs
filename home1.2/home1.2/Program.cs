aedc90f6c8bed762fc62b99c59a7443c78cc98e6
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-04-26 18:59:23 +0300

    Add files via upload

---
+++ home1.2/home1.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = GetValidInput("Введите делимое: ");
+        int b = GetValidInput("Введите делитель: ");
+
+        // Проверка деления на нуль
+        while (b == 0)
+        {
+            Console.WriteLine("Ошибка: делитель не может быть нулем. Пожалуйста, введите делитель еще раз.");
+            b = GetValidInput("Введите делитель: ");
+        }
+
+        Del(a, b);
+    }
+
+    static int GetValidInput(string message)
+    {
+        int result = 0;
+        bool isValidInput = false;
+
+        while (!isValidInput)
+        {
+            Console.Write(message);
+            string userInput = Console.ReadLine();
+
+            isValidInput = int.TryParse(userInput, out result);
+
+            if (!isValidInput)
+            {
+                Console.WriteLine("Ошибка! Введите корректное целое число:");
+            }
+        }
+        return result;
+    }
+
+    static void Del(int A, int B)
+    {
+        bool isNegativeResult = (A < 0) ^ (B < 0); // Если в одном из чисел стоит отрицательный знак
+        int n;
+
+        A = Math.Abs(A);
+        B = Math.Abs(B);
+
+        for (n = 0; A >= B; n++)
+        {
+            A -= B;
+        }
+
+        if (isNegativeResult)
+        {
+            n = -n; // Знак частного зависит от знаков делимого и делителя
+        }
+
+        Console.WriteLine($"Целая часть {n}, остаток {A}");
+    }
+}
