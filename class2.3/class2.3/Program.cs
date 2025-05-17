d8f2f0049ca09d2451ab4f47e781ece00b4eed33
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-05-17 22:08:23 +0300

    Add files via upload

---
+++ class2.3/class2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите любое целое число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        func(n);
+    }
+    static void func(int num)
+    {
+        int q = 0;
+        while (num > 1)
+        {
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+                q += 1;
+                Console.WriteLine(num);
+            }
+            else
+            {
+                num = 3 * num + 1;
+                q += 1;
+                Console.WriteLine(num);
+            }
+        }
+        Console.WriteLine($"Количество замен, приводящих к 1: {q}");
+       
+    }
+}
