4c45b2e429b6658d97a879567f5d9520e66936ad
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-05-20 23:23:19 +0300

    Add files via upload

---
+++ class2.9/class2.9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первый параметр функции: ");
+        double parametr1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второй параметр функции: ");
+        double parametr2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите третий параметр функции: ");
+        double parametr3 = Convert.ToDouble(Console.ReadLine());
+        ThreeParam(parametr1, parametr2, parametr3);
+    }
+    static void ThreeParam(double p1, double p2, double p3)
+    {
+        p1 = (p2 + p3) / 2;
+        Console.WriteLine($"Значение первого параметра получилось равным {p1}.");
+        p2 = (p1 + p3) / 2;
+        Console.WriteLine($"Значение второго параметра получилось равным {p2}.");
+        p3 = (p1 + p2) / 2;
+        Console.WriteLine($"Значение третьего параметра получилось равным {p3}.");
+    }
+}
