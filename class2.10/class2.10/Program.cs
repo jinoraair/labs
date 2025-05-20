c27bdcf0ff5976439b28ba71ce761cf76aefcd45
jinoraair <100207241+jinoraair@users.noreply.github.com>
2025-05-20 23:31:58 +0300

    Add files via upload

---
+++ class2.10/class2.10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первый параметр функции: ");
+        double parametr1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второй параметр функции: ");
+        double parametr2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите третий параметр функции: ");
+        double parametr3 = Convert.ToDouble(Console.ReadLine());
+        ThreeParam(parametr1, parametr2, parametr3);
+    }
+    static void ThreeParam(double p1, double p2, double p3)
+    {
+        double result = (p1 + p2 + p3) / 3;
+        Console.WriteLine($"Среднее арифметическое трех параметров: {result}");
+    }
+}
